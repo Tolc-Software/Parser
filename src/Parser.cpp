75da343c454272ce1474ac69a21a3b0816e30c5e
srydell <simonwrydell@gmail.com>
2019-12-10 23:12:12 +0100

    Added the AddClang cmake script
    * Solved error on includes with: target_compile_options(Parser PRIVATE --system-header-prefix=clang/)

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,27 +1,57 @@
-#include "clang/Frontend/FrontendActions.h"
-#include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/AST/ASTConsumer.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/FrontendAction.h"
 #include "clang/Tooling/Tooling.h"
-// Declares llvm::cl::extrahelp.
-#include "llvm/Support/CommandLine.h"
 
-using namespace clang::tooling;
-using namespace llvm;
+using namespace clang;
 
-// Apply a custom category to all command-line options so that they are the
-// only ones displayed.
-static llvm::cl::OptionCategory MyToolCategory("my-tool options");
+class FindNamedClassVisitor
+    : public RecursiveASTVisitor<FindNamedClassVisitor> {
+public:
+	explicit FindNamedClassVisitor(ASTContext* Context) : Context(Context) {}
 
-// CommonOptionsParser declares HelpMessage with a description of the common
-// command-line options related to the compilation database and input files.
-// It's nice to have this help message in all tools.
-static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
+	bool VisitCXXRecordDecl(CXXRecordDecl* Declaration) {
+		if (Declaration->getQualifiedNameAsString() == "n::m::C") {
+			FullSourceLoc FullLocation =
+			    Context->getFullLoc(Declaration->getBeginLoc());
+			if (FullLocation.isValid())
+				llvm::outs() << "Found declaration at "
+				             << FullLocation.getSpellingLineNumber() << ":"
+				             << FullLocation.getSpellingColumnNumber() << "\n";
+		}
+		return true;
+	}
 
-// A help message for this specific tool can be added afterwards.
-static cl::extrahelp MoreHelp("\nMore help text...\n");
+private:
+	ASTContext* Context;
+};
 
-int main(int argc, const char** argv) {
-	CommonOptionsParser OptionsParser(argc, argv, MyToolCategory);
-	ClangTool Tool(OptionsParser.getCompilations(),
-	               OptionsParser.getSourcePathList());
-	return Tool.run(newFrontendActionFactory<clang::SyntaxOnlyAction>().get());
+class FindNamedClassConsumer : public clang::ASTConsumer {
+public:
+	explicit FindNamedClassConsumer(ASTContext* Context) : Visitor(Context) {}
+
+	virtual void HandleTranslationUnit(clang::ASTContext& Context) {
+		Visitor.TraverseDecl(Context.getTranslationUnitDecl());
+	}
+
+private:
+	FindNamedClassVisitor Visitor;
+};
+
+class FindNamedClassAction : public clang::ASTFrontendAction {
+public:
+	virtual std::unique_ptr<clang::ASTConsumer>
+	CreateASTConsumer(clang::CompilerInstance& Compiler,
+	                  llvm::StringRef /*InFile*/) {
+		return std::unique_ptr<clang::ASTConsumer>(
+		    new FindNamedClassConsumer(&Compiler.getASTContext()));
+	}
+};
+
+int main(int argc, char** argv) {
+	if (argc > 1) {
+		auto n = std::make_unique<FindNamedClassAction>();
+		clang::tooling::runToolOnCode(std::move(n), argv[1]);
+	}
 }
