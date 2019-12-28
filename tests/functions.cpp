428f96fe4fc5aababe2390666f710b71f78a41e5
srydell <simonwrydell@gmail.com>
2019-12-28 11:54:20 +0100

    Added cmake modules for handling catch2 tests
    * Added a simle test functions to build on later

---
+++ tests/functions.cpp
@@ -0,0 +1,13 @@
+#include "Frontend/ParserFrontendAction.h"
+#include "catch2/catch.hpp"
+#include "clang/Tooling/Tooling.h"
+
+TEST_CASE("Run tool", "[functions]") {
+	clang::tooling::runToolOnCode(new Frontend::ParserFrontendAction(),
+	                              R"(
+int FunctionName(const double d, int i) {
+	return d + 5;
+}
+		)");
+	REQUIRE(1 != 1);
+}
