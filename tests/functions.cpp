83348deec20cb7e05fc0398d8033bdcd20892a4c
srydell <simonwrydell@gmail.com>
2019-12-30 18:47:06 +0100

    Divided up functions test

--- tests/functions.cpp
+++ tests/functions.cpp
@@ -2,10 +2,44 @@
 #include "catch2/catch.hpp"
 #include "clang/Tooling/Tooling.h"
 
-TEST_CASE("Run tool", "[functions]") {
+TEST_CASE("Simple function", "[functions]") {
 	clang::tooling::runToolOnCode(new Frontend::ParserFrontendAction(),
 	                              R"(
-int FunctionName(const double d, int i) {
+void FunctionName() {}
+		)");
+}
+
+TEST_CASE("With return type", "[functions]") {
+	clang::tooling::runToolOnCode(new Frontend::ParserFrontendAction(),
+	                              R"(
+int FunctionName() {
+	return 5;
+}
+		)");
+}
+
+TEST_CASE("Return type and simple argument", "[functions]") {
+	clang::tooling::runToolOnCode(new Frontend::ParserFrontendAction(),
+	                              R"(
+int FunctionName(int i) {
+	return i;
+}
+		)");
+}
+
+TEST_CASE("Return type with const qualifier", "[functions]") {
+	clang::tooling::runToolOnCode(new Frontend::ParserFrontendAction(),
+	                              R"(
+int FunctionName(const int i) {
+	return i;
+}
+		)");
+}
+
+TEST_CASE("Multiple return types", "[functions]") {
+	clang::tooling::runToolOnCode(new Frontend::ParserFrontendAction(),
+	                              R"(
+int FunctionName(const int i, double d) {
 	return d + 5;
 }
 		)");
