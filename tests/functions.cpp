20c04dc976a5175a25495ed0aec5f7f6f880b56c
srydell <simonwrydell@gmail.com>
2020-02-13 18:24:03 +0100

    Added tests for default access modifiers

--- tests/functions.cpp
+++ tests/functions.cpp
@@ -6,6 +6,26 @@
 #include <catch2/catch.hpp>
 #include <variant>
 
+TEST_CASE("Function works with default modifier", "[functions]") {
+	using IR::AccessModifier;
+	for (auto [accessModifier, structure] :
+	     {std::make_pair(AccessModifier::Private, std::string("class")),
+	      std::make_pair(AccessModifier::Public, std::string("struct"))}) {
+		auto globalNS =
+		    Parser::parseString(structure + " MyStructure { void fun(); };");
+
+		SECTION("Parser finds the function") {
+			REQUIRE(globalNS.m_functions.size() == 0);
+			REQUIRE(globalNS.m_structs.size() == 1);
+			auto structure = globalNS.m_structs[0];
+			REQUIRE(structure.m_functions.size() == 1);
+			auto& [access, fun] = structure.m_functions.back();
+			CHECK(fun.m_name == "fun");
+			CHECK(access == accessModifier);
+		}
+	}
+}
+
 TEST_CASE("Function within class with modifier", "[functions]") {
 	for (auto accessModifier : TestUtil::getAccessModifiers()) {
 		auto globalNS = Parser::parseString(
