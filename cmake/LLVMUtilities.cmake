fbf7238b0e62b2c55e2a92b3ad14b9f75877e86d
srydell <simonwrydell@gmail.com>
2020-03-12 20:41:29 +0100

    Generate correct include path to system libraries in libtooling
    * Added a VERSION argument to setup_llvm

--- cmake/LLVMUtilities.cmake
+++ cmake/LLVMUtilities.cmake
@@ -4,18 +4,22 @@ function(setup_llvm)
   # Define the supported set of keywords
   set(prefix ARG)
   set(noValues)
-  set(singleValues NAME)
+  set(singleValues NAME VERSION)
   set(multiValues)
   # Process the arguments passed in
   # can be used e.g. via ARG_NAME
-  cmake_parse_arguments(${prefix}
-                        "${noValues}"
-                        "${singleValues}"
-                        "${multiValues}"
-                        ${ARGN})
+  cmake_parse_arguments(${prefix} "${noValues}" "${singleValues}"
+                        "${multiValues}" ${ARGN})
 
   if(NOT ARG_NAME)
-    message(FATAL_ERROR "Must provide a name. e.g. setupLLVM(NAME LLVM_ENTRY)")
+    message(
+      FATAL_ERROR
+        "Must provide a name. e.g. setupLLVM(NAME LLVM_ENTRY VERSION 9.0.0)")
+  endif()
+  if(NOT ARG_VERSION)
+    message(
+      FATAL_ERROR
+        "Must provide a version. e.g. setupLLVM(NAME LLVM_ENTRY VERSION 9.0.0)")
   endif()
 
   message(STATUS "Got name: ${ARG_NAME}")
@@ -27,15 +31,13 @@ function(setup_llvm)
   if(${CMAKE_HOST_SYSTEM_NAME} STREQUAL Linux)
     FetchContent_Declare(
       ${llvm_entry}
-      URL
-        http://releases.llvm.org/9.0.0/clang+llvm-9.0.0-x86_64-pc-linux-gnu.tar.xz
-      )
+      URL http://releases.llvm.org/${ARG_VERSION}/clang+llvm-${ARG_VERSION}-x86_64-pc-linux-gnu.tar.xz
+    )
   elseif(${CMAKE_HOST_SYSTEM_NAME} STREQUAL Darwin)
     FetchContent_Declare(
       ${llvm_entry}
-      URL
-        http://releases.llvm.org/9.0.0/clang+llvm-9.0.0-x86_64-darwin-apple.tar.xz
-      )
+      URL http://releases.llvm.org/${ARG_VERSION}/clang+llvm-${ARG_VERSION}-x86_64-darwin-apple.tar.xz
+    )
   else()
     message(FATAL_ERROR "Unsupported platform for now.")
   endif()
@@ -48,7 +50,9 @@ function(setup_llvm)
   set(${llvm_entry}_INCLUDE_DIR
       ${${llvm_entry}_SOURCE_DIR}/include
       PARENT_SCOPE)
-  set(${llvm_entry}_SOURCE_DIR ${${llvm_entry}_SOURCE_DIR} PARENT_SCOPE)
+  set(${llvm_entry}_SOURCE_DIR
+      ${${llvm_entry}_SOURCE_DIR}
+      PARENT_SCOPE)
 endfunction()
 
 macro(get_clang_and_llvm)
@@ -59,35 +63,34 @@ macro(get_clang_and_llvm)
   set(multiValues)
   # Process the arguments passed in
   # can be used e.g. via ARG_PATH
-  cmake_parse_arguments(${prefix}
-                        "${noValues}"
-                        "${singleValues}"
-                        "${multiValues}"
-                        ${ARGN})
+  cmake_parse_arguments(${prefix} "${noValues}" "${singleValues}"
+                        "${multiValues}" ${ARGN})
 
   if(NOT ARG_PATH)
     message(
       FATAL_ERROR
         "Must provide a path to the clang installation. e.g. getClang(PATH $\{llvm_entry_SOURCE_DIR\})"
-      )
+    )
   endif()
 
   set(Clang_ROOT ${ARG_PATH})
-  find_package(Clang
-               REQUIRED
-               CONFIG
-               PATHS
-               ${Clang_ROOT}
-               REQUIRED
-               NO_DEFAULT_PATH)
+  find_package(
+    Clang
+    REQUIRED
+    CONFIG
+    PATHS
+    ${Clang_ROOT}
+    REQUIRED
+    NO_DEFAULT_PATH)
 
   set(LLVM_ROOT ${ARG_PATH})
-  find_package(LLVM
-               REQUIRED
-               CONFIG
-               PATHS
-               ${LLVM_ROOT}
-               REQUIRED
-               NO_DEFAULT_PATH)
+  find_package(
+    LLVM
+    REQUIRED
+    CONFIG
+    PATHS
+    ${LLVM_ROOT}
+    REQUIRED
+    NO_DEFAULT_PATH)
   list(APPEND CMAKE_MODULE_PATH "${LLVM_CMAKE_DIR}")
 endmacro()
--- src/Parser/Parse.cpp
+++ src/Parser/Parse.cpp
@@ -1,16 +1,9 @@
 #include "Parser/Parse.h"
 #include "Frontend/ParserFrontendAction.h"
+#include "Helpers/includes.h"
 #include "clang/Tooling/Tooling.h"
 #include <iostream>
 #include <string>
-
-namespace {
-std::vector<std::string> getIncludes() {
-	// return {"-I/usr/lib/clang/9.0.1/include/"};
-	return {
-	    "-I/home/simon/external/code/cpp/Parser/build/_deps/llvm_entry-src/lib/clang/9.0.0/include"};
-}
-}    // namespace
 
 namespace Parser {
 
@@ -19,13 +12,11 @@ IR::Namespace parseString(std::string const& code) {
 	std::cout << "Parsing code:" << '\n';
 	std::cout << code << '\n';
 	IR::Namespace parsedIR;
-	// clang::tooling::runToolOnCode(new Frontend::ParserFrontendAction(parsedIR),
-	//                               code.c_str());
 
 	clang::tooling::runToolOnCodeWithArgs(
 	    new Frontend::ParserFrontendAction(parsedIR),
 	    code.c_str(),
-	    getIncludes());
+	    Helpers::getSystemIncludes());
 
 	return parsedIR;
 }
