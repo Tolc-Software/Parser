be74d13eedde1924f49a07c3c69b9a03384a2bc1
srydell <simonwrydell@gmail.com>
2020-03-15 17:25:10 +0100

    template for a test of user defined types

--- tests/userDefined.cpp
+++ tests/userDefined.cpp
@@ -1,5 +1,16 @@
+#include "TestUtil/parse.h"
 #include <catch2/catch.hpp>
 
 TEST_CASE("A simple user defined struct", "[userDefined]") {
 	REQUIRE(1 == 1);
+	auto globalNS = TestUtil::parseString(R"(
+struct Simple {};
+
+Simple f();
+)");
+	SECTION("Parser finds an empty struct named Simple") {
+		REQUIRE(globalNS.m_functions.size() == 1);
+		auto& f = globalNS.m_functions[0];
+		REQUIRE(f.m_name == "f");
+	}
 }
