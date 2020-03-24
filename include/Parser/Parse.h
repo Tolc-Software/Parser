e8e8a5c5f40888c1a9eed4fdef89ef2124dd3162
srydell <simonwrydell@gmail.com>
2020-03-24 20:43:19 +0100

    Added simple factory for parsing files

--- include/Parser/Parse.h
+++ include/Parser/Parse.h
@@ -5,11 +5,20 @@
 
 namespace Parser {
 /**
+* @brief: Parse the input file as C++ code and return the intermediate representation.
+*
+* @param: std::string const& filename
+*
+* @return: IR::Namespace - The global namespace
+*/
+IR::Namespace parseFile(std::string const& filename);
+
+/**
 * @brief: Parse the input as C++ code and return the intermediate representation.
 *
 * @param: std::string const& code
 *
-* @return: IR::Namespace
+* @return: IR::Namespace - The global namespace
 */
 IR::Namespace parseString(std::string const& code);
 }
--- src/Parser/Parse.cpp
+++ src/Parser/Parse.cpp
@@ -1,10 +1,41 @@
 #include "Parser/Parse.h"
 #include "Frontend/ParserFrontendAction.h"
 #include "Helpers/includes.h"
+#include "clang/Tooling/CompilationDatabase.h"
 #include "clang/Tooling/Tooling.h"
 #include <string>
 
+std::unique_ptr<clang::tooling::FrontendActionFactory>
+newParserFrontendActionFactory(IR::Namespace& ns) {
+	class ParserFrontendActionFactory
+	    : public clang::tooling::FrontendActionFactory {
+	public:
+		ParserFrontendActionFactory(IR::Namespace& parsedNamespaces)
+		    : m_parsedNamespaces(parsedNamespaces) {}
+		IR::Namespace& m_parsedNamespaces;
+		clang::FrontendAction* create() override {
+			return new Frontend::ParserFrontendAction(m_parsedNamespaces);
+		}
+	};
+
+	return std::unique_ptr<clang::tooling::FrontendActionFactory>(
+	    new ParserFrontendActionFactory(ns));
+}
+
 namespace Parser {
+IR::Namespace parseFile(std::string const& filename) {
+	IR::Namespace parsedIR;
+
+	// Create the db for flags
+	std::string fromDirectory = ".";
+	clang::tooling::FixedCompilationDatabase compDb(
+	    fromDirectory, Helpers::getSystemIncludes());
+
+	clang::tooling::ClangTool tool(compDb, {filename});
+	// TODO: Create a new factory by inheriting from newFrontendActionFactory
+	tool.run(newParserFrontendActionFactory(parsedIR).get());
+	return parsedIR;
+}
 
 IR::Namespace parseString(std::string const& code) {
 	IR::Namespace parsedIR;
