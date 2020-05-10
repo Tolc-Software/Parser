8b9c8df47be21220cc218116d0b8bc34345f3c0d
srydell <simonwrydell@gmail.com>
2020-05-10 18:25:14 +0200

    Add an example of how to extract template arguments as qualtypes for qualtypes

--- src/Builders/typeBuilder.cpp
+++ src/Builders/typeBuilder.cpp
@@ -3,13 +3,48 @@
 #include "Helpers/Type/utilities.hpp"
 #include "Helpers/Utils/string.hpp"
 #include "IRProxy/IRData.hpp"
+#include <clang/AST/Decl.h>
+#include <clang/AST/DeclTemplate.h>
 #include <clang/AST/Type.h>
+#include <iostream>
+#include <llvm/Support/Casting.h>
+#include <vector>
 
 namespace Builders {
+
+// generate vector<QualType> with all types that class uses if it tempalated
+std::vector<clang::QualType> get_type_dependencies(
+    clang::CXXRecordDecl const* C /*, bool include_members*/) {
+	std::vector<clang::QualType> r;
+
+	std::cout << "Checking for specialization" << '\n';
+	if (auto t = llvm::dyn_cast<clang::ClassTemplateSpecializationDecl>(C)) {
+		std::cout << "It is a class specialization!" << '\n';
+		for (unsigned int i = 0; i < t->getTemplateArgs().size(); ++i) {
+			if (t->getTemplateArgs()[i].getKind() ==
+			    clang::TemplateArgument::Type) {
+				r.push_back(
+				    t->getTemplateArgs()[i]
+				        .getAsType() /*.getDesugaredType(C->getASTContext())*/);
+			}
+		}
+	}
+
+	for (auto const& t : r) {
+		std::cout << t.getAsString() << '\n';
+	}
+
+	return r;
+}
 
 std::optional<IR::Type> buildType(clang::QualType type) {
 	// What the user wrote
 	auto representation = type.getAsString();
+
+	std::cout << "Processing representation: " << representation << '\n';
+	if (auto record = type->getAsCXXRecordDecl()) {
+		get_type_dependencies(record);
+	}
 
 	// Remove using aliases
 	type = type.getCanonicalType();
