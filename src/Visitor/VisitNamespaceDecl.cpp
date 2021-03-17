3b0396b6be21faee8ad89804659b8e3dae8780c5
Simon Rydell <simonwrydell@gmail.com>
2021-03-17 16:36:50 +0100

    Remove debug statement

--- src/Visitor/VisitNamespaceDecl.cpp
+++ src/Visitor/VisitNamespaceDecl.cpp
@@ -1,7 +1,6 @@
 #include "Visitor/ParserVisitor.hpp"
 #include "clang/AST/DeclBase.h"
 #include "clang/AST/Type.h"
-#include <iostream>
 #include <spdlog/spdlog.h>
 
 namespace Visitor {
@@ -13,9 +12,6 @@ bool ParserVisitor::VisitNamespaceDecl(clang::NamespaceDecl* namespaceDecl) {
 	}
 	spdlog::debug("Parsing namespace: {}",
 	              namespaceDecl->getQualifiedNameAsString());
-
-	std::cout << "On namespace: " << namespaceDecl->getQualifiedNameAsString()
-	          << '\n';
 
 	// Export our parsed namespace
 	m_irData.m_namespaces.push_back(namespaceDecl->getQualifiedNameAsString());
